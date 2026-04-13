e68739514f35e1b8d0e36773b180f60f8bf9a10b
merlovesthemonkeys-sketch <merlovesthemonkeys@gmail.com>
2026-04-13 16:44:32 +1000

    UI Updates
    Panel background transparency, text positions & Camera angles.

--- Assets/MeredithsFolder/MeredithsScripts/CameraFollow.cs
+++ Assets/MeredithsFolder/MeredithsScripts/CameraFollow.cs
@@ -3,14 +3,15 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
-    public Vector3 offset = new Vector3(0f, 8f, -8f);
+    public Vector3 offset = new Vector3(0f, 7f, -6f);
+    public Vector3 lookOffset = new Vector3(0f, 1.5f, 0f);
 
     void LateUpdate()
     {
         if (player != null)
         {
             transform.position = player.position + offset;
-            transform.LookAt(player);
+            transform.LookAt(player.position + lookOffset);
         }
     }
 }
