91937d6c8df6bbb2590b4b188a86f788b34cf574
lucygeorge2807-creator <lucy.george2807@gmail.com>
2026-04-18 10:31:29 +1000

    Lucys Scene
    Added score system.

--- Assets/LucysFolder/LucysScripts/PlayerController_Lucy.cs
+++ Assets/LucysFolder/LucysScripts/PlayerController_Lucy.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController_Lucy : MonoBehaviour
 {
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
 
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update
     void Start()
     {
-         rb = GetComponent<Rigidbody>();  
+         rb = GetComponent<Rigidbody>(); 
+         count = 0; 
+         SetCountText();
+         winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -26,6 +33,15 @@ public class PlayerController_Lucy : MonoBehaviour
         movementY = movementVector.y;
     }
 
+void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+       
+        if (count >= 5)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
     void FixedUpdate()
     {
         Vector3 movement = new Vector3 (movementX,0.0f, movementY);  
@@ -36,7 +52,9 @@ public class PlayerController_Lucy : MonoBehaviour
     {
         if(other.gameObject.CompareTag("PickUp"))
         {
-        other.gameObject.SetActive(false);    
+        other.gameObject.SetActive(false); 
+        count = count + 1;  
+        SetCountText();
         }
     }
 }
