d129a5c14b1f70a7f63eb4d94552ee92bec31b4f
lucygeorge2807-creator <lucy.george2807@gmail.com>
2026-04-20 10:17:08 +1000

    Lucys scene
    Updated success panel

---
+++ Assets/LucysFolder/LucysScripts/UIController_Lucy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController_Lucy : MonoBehaviour
+{
+    public GameObject panel; 
+    public void HidePanel()
+    {
+      panel.SetActive(false);  
+    }   
+}
